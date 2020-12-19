28c0793448c491a0755bd57da690e37bc627ac12
frimpongOpoku <pongofrimi@gmail.com>
2020-12-19 20:40:16 +0000

    Initial Commit

---
+++ playingWithJSON/playingWithJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  playingWithJSONApp.swift
+//  playingWithJSON
+//
+//  Created by Frimpong Opoku on 19/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct playingWithJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
